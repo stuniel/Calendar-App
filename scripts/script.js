39a260d247d8cdceb6d6588007e8ebb99cc4ea58
stuniel <stuniel@gmail.com>
2017-07-04 16:42:19 +0200

    Added some responsive features like css media quries. Created functionality that allows to reorder tasks by clicking and dragging.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+const container = document.querySelector('.container');
 const nowMonth = document.querySelector('.now-month');
 const prevMonth = document.querySelector('.prev-month');
 const nextMonth = document.querySelector('.next-month');
@@ -12,10 +13,12 @@ const addedTasks = document.querySelector('.added-tasks');
 const form = document.querySelector('.task-form');
 const input = document.querySelector('.add-task-input');
 const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
-let day;
+let day, draggedElement, draggedElementObject, overDraggedElement, overDraggedElementObject, shadowTask;
+let cursorStart, cursorMove;
 let dayParse;
-let uniqueNumber = 0;
-
+let lastClicked = weeks[0];
+let taskMoveFlag = false;
+let printedTasks = [];
 
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const weekDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
@@ -69,6 +72,7 @@ function setDate() {
 
 function drawDaysTable(chosenMonth) {
   const weekDay = chosenMonth.getDay();
+  days.forEach(day => day.classList.remove('clicked-day-highlight'));
 
   const numberOfDays = new Date(currentYear, currentMonth - 1, 0).getDate();
   let weekNumber = 1;
@@ -91,9 +95,11 @@ function drawDaysTable(chosenMonth) {
 function clickedDay() {
   day = new Date(currentYear, currentMonth - 1, this.innerText);
   dayParse = Date.parse(day);
+  lastClicked.classList.remove('clicked-day-highlight');
+  this.classList.add('clicked-day-highlight');
+  lastClicked = this;
   drawTasks(day);
   showTasks(dayParse);
-  console.log(day);
 }
 
 function drawTasks(day) {
@@ -116,38 +122,130 @@ function createTask(e) {
     name: this.querySelector('[name=task]').value,
     date: day,
     dateParse: Date.parse(day),
-    number: uniqueNumber
+    number: (function () {
+      let password = "";
+      const passwordLetters = "qwertyuiopasdfghjklzxcvbnm1234567890";
+      for (i = 0; i < 12; i++) {
+        password += passwordLetters.charAt(Math.random() * passwordLetters.length);
+      }
+      return password;
+    })()
   }
-  uniqueNumber++;
   taskList.push(task);
   localStorage.setItem('taskList', JSON.stringify(taskList));
   showTasks(dayParse);
   highlightToday(day);
   this.reset();
+  if (window.innerWidth < 501) {
+    addedTasks.style.height = (addedTasks.clientHeight + 50) + "px";
+  }
+  console.table(taskList);
 }
 
 function showTasks(today) {
-  console.log(today, "hi");
   addedTasks.innerHTML = taskList
     .filter(task => {
       return task.dateParse == today;
     })
     .map(function(task) {
-      return `<div class="task" data-number=${task.number}><span class="task-name">${task.name}</span><span class="delete-task">&#128929;</span></div>`;
+      return `<div class="task draggable" data-number=${task.number}><span class="task-name">${task.name}</span><span class="delete-task">&#128929;</span></div>`;
     }).join('');
-    document.querySelectorAll('.delete-task').forEach(del => del.addEventListener('click', deleteTask));
+  printedTasks = document.querySelectorAll('.task');
+  document.querySelectorAll('.delete-task').forEach(del => del.addEventListener('click', deleteTask));
+
+  printedTasks.forEach(printedTask => printedTask.addEventListener('mousedown', startDragging));
+  printedTasks.forEach(printedTask => printedTask.addEventListener('touchstart', startDragging));
+  printedTasks.forEach(printedTask => printedTask.addEventListener('dragstart', function(e) {
+    e.dataTransfer.setData('text/plain', 'node');
+  }));
+  printedTasks.forEach(printedTask => printedTask.addEventListener('drag', continueDragging));
+  printedTasks.forEach(printedTask => printedTask.addEventListener('touchmove', continueDraggingTouch));
+  window.addEventListener('mouseout', function(e) {
+    if(taskMoveFlag) {
+      console.log('hello', shadowTask)
+      printedTasks.forEach(printedTask => printedTask.classList.remove('dragged-task'))
+      taskMoveFlag = false;
+      localStorage.setItem('taskList', JSON.stringify(taskList));
+    }
+  });
+
+  function startDragging(e) {
+    printedTasks.forEach(printedTask => {
+      if (printedTask.dataset.number == this.dataset.number) {
+        shadowTask = printedTask;
+        shadowTask.classList.add('dragged-task');
+      }
+    })
+    taskMoveFlag = true;
+    taskList.forEach(task => { if(task.number == this.dataset.number) draggedElementObject = task })
+  }
+
+  function continueDragging(e) {
+    if (!taskMoveFlag) return;
+    else {
+      printedTasks.forEach(printedTask => {
+        if (printedTask != this && e.clientY < (printedTask.offsetTop + printedTask.clientHeight) && e.clientY > printedTask.offsetTop) {
+          overDraggedElement = printedTask;
+          taskList.forEach(task => { if(task.number == overDraggedElement.dataset.number) overDraggedElementObject = task })
+          taskList.move(taskList.indexOf(draggedElementObject), taskList.indexOf(overDraggedElementObject));
+        }
+      })
+    }
+    showTasks(dayParse);
+    printedTasks.forEach(printedTask => {
+      if (printedTask.dataset.number == this.dataset.number) {
+        shadowTask = printedTask;
+        shadowTask.classList.add('dragged-task');
+      }
+    })
+  }
+
+  // For touch screens
+
+  function continueDraggingTouch(e) {
+    const touch = event.touches[0]
+    printedTasks.forEach(printedTask => {
+      if (taskMoveFlag && printedTask != this && touch.pageY < (printedTask.offsetTop + printedTask.clientHeight) && touch.pageY > printedTask.offsetTop) {
+        overDraggedElement = printedTask;
+        taskList.forEach(task => { if(task.number == overDraggedElement.dataset.number) overDraggedElementObject = task })
+        taskList.move(taskList.indexOf(draggedElementObject), taskList.indexOf(overDraggedElementObject));
+      }
+    })
+    showTasks(dayParse);
+  }
+
+  // Close cross
+
+  printedTasks.forEach(printedTask => printedTask.addEventListener('mouseover', function(e) {
+    this.querySelector('.delete-task').classList.add('cross-visible');
+  }));
+  printedTasks.forEach(printedTask => printedTask.addEventListener('mouseout', closeCrossInvisible));
+}
+
+
+
+
+// function closeCrossVisible(e) {
+//   this.querySelector('.delete-task').classList.add('cross-visible');
+// }
+
+function closeCrossInvisible(e) {
+  this.querySelector('.delete-task').classList.remove('cross-visible');
 }
 
 function deleteTask(e) {
   const elmNumber = this.parentElement.dataset.number;
   taskList.forEach(task => {
     if (elmNumber == task.number) {
+      console.log(this.parentNode);
+      this.parentElement.classList.add('removed-task');
       const elmIndex = taskList.indexOf(task);
+      this.parentElement.parentNode.removeChild(this.parentElement);
+      console.log(this.parentNode);
       taskList.splice((elmIndex), 1);
       localStorage.setItem('taskList', JSON.stringify(taskList));
-      this.parentElement.parentNode.removeChild(this.parentElement);
-      console.log("Deleted");
-    }
+      highlightToday(day);
+      }
   });
 }
 
@@ -165,9 +263,9 @@ function highlightToday(date) {
     const dayCheck = Date.parse(new Date(date.getFullYear(), date.getMonth(), weekday.innerText));
     const todayCheck = Date.parse(new Date(temporaryToday.getFullYear(), temporaryToday.getMonth(), temporaryToday.getDate()));
     if (dayCheck === todayCheck && weekday.innerText != "") {
-      weekday.classList.add('today-circle');
+      weekday.classList.add('today-background');
     } else {
-      weekday.classList.remove('today-circle');
+      weekday.classList.remove('today-background');
     }
     taskList.forEach(task => {
       if (dayCheck == task.dateParse) {
@@ -203,5 +301,11 @@ if (!Array.prototype.indexOf) {
             return i;
     }
     return -1;
+  };
+}
+
+if (!Array.prototype.move) {
+  Array.prototype.move = function (from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
   };
 }
